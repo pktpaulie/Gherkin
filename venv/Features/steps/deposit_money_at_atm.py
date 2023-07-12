b718595703eddf045f6176eaa84ee163fa568860
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-07-13 01:25:47 +0300

    successful deposit feature

--- venv/Features/steps/deposit_money_at_atm.py
+++ venv/Features/steps/deposit_money_at_atm.py
@@ -16,3 +16,27 @@ def account_is_locked(context, status):
     context.status = str(status)
 
 #successful deposit
+@when('I deposit UGX {deposit_amount}')
+def successful_deposit(context, deposit_amount):
+    context.deposit_amount = int(deposit_amount)
+    remaining_balance = deposit(context)
+    assert remaining_balance == context.initial_balance + (context.deposit_amount - context.transaction_fees)
+
+def deposit(context):
+    my_balance = context.initial_balance + (context.deposit_amount - context.transaction_fees)
+    return my_balance
+
+#successful deposit
+@when('Account state is "{status}"')
+def account_state(context, status):
+    if status == "unlocked":
+        context.successful_deposit = True
+    elif status == "locked":
+        context.successful_deposit = False
+
+#successful deposit
+@then('Feedback reads "Successful"')
+def feedback(context):
+    assert context.successful_deposit is True
+
+
