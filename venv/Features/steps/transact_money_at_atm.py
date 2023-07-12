a68d038b07b25cbcc01cfd406551d3c26834f3ed
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-07-13 00:31:34 +0300

    passed scenarios

--- venv/Features/steps/transact_money_at_atm.py
+++ venv/Features/steps/transact_money_at_atm.py
@@ -20,16 +20,29 @@ def minimum_balance(context, min_balance):
 def successful_withdrawal(context, amount):
     context.withdraw = int(amount)
     remaining = withdraw(context)
-    assert remaining == context.initial_balance 
+    assert remaining == context.initial_balance - (context.withdraw + context.transaction_fees)
 
 def withdraw(context):
-    initial_balance = context.initial_balance 
+    initial_balance = context.initial_balance - (context.withdraw + context.transaction_fees)
     return initial_balance
 
 
 @then('Account balance is {balance}')
 def account_balance_after_withdrawal(context, balance):
     context.balance = int(balance)
+    assert context.balance == context.initial_balance - (context.withdraw + context.transaction_fees)
+
+
+#failed withdrawal
+@when('I fail withdraw UGX {withdraw_amount}')
+def failed_withdraw(context, withdraw_amount):
+    context.withdraw = int(withdraw_amount)
+    if context.withdraw >= context.min_balance:
+        context.failed_transaction = True
+    else:
+        context.failed_transaction = False
+
+@then('failed Account balance is {initial_balance}')
+def failed_withdrawal(context, initial_balance):
+    context.balance = int(initial_balance)
     assert context.balance == context.initial_balance
-
-
